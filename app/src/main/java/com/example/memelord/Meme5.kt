358db637b84a6ec50df48b52b72ca5b0138fc686
ReginaChege <reginachegewairimu@gmail.com>
2023-07-05 08:32:51 +0300

    memeapp

---
+++ app/src/main/java/com/example/memelord/Meme5.kt
@@ -0,0 +1,11 @@
+package com.example.memelord
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Meme5 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meme5)
+    }
+}
